fcda6122d85fa4a065b5fdb77d055775e04ad232
luis paredes <luis paredes>
2017-04-09 12:10:32 -0400

    created sales page

---
+++ assets/javascript/js-sales.js
@@ -0,0 +1,232 @@
+$(document).ready(function($) {
+	var config = {
+		apiKey: "AIzaSyD6Dify4yji4waHQ-BZxwwvK-UHrfZdOlk",
+	    authDomain: "project-1-sales.firebaseapp.com",
+	    databaseURL: "https://project-1-sales.firebaseio.com",
+	    projectId: "project-1-sales",
+	    storageBucket: "project-1-sales.appspot.com",
+	    messagingSenderId: "933119782430"
+	};
+
+	firebase.initializeApp(config);
+
+	var database = firebase.database();
+	var imgName = "";
+	var updateImgName = "";
+	var n = 0;
+	var m = 0;
+	var sortedAsc = false;
+	var sortedDesc = false;
+
+	// Add item to table
+	database.ref().on("child_added", function(snapshot) {
+		var child = snapshot.val();
+		var key = snapshot.key;
+
+		if (child.status === "Open") {
+			// Create row
+			$("#open-table tbody").append("<tr data-key=\"" + key + "\"><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>");
+
+			// Add values to corresponding column
+			var rowTds = $("#open-table")
+				.children()
+				.eq(1)
+				.children("tr")
+				.eq(n)
+				.children("td");
+			var headings = [child.orderNumber, child.created, child.name, child.amount, child.description, child.card, child.last4];
+			for (var i = 0; i < headings.length; i++) {
+				rowTds.eq(i).text(headings[i]);
+			}
+
+			// Append Remove Button
+			var removeButton = $("<button>");
+			removeButton.attr("type", "button");
+			removeButton.attr("data-key", key);
+			removeButton.attr("class", "btn btn-warning picked-up-button");
+			removeButton.text("Picked Up");
+			$("#open-table tbody tr:last td:last").append(removeButton);
+
+			n++;
+		} else if (child.status === "Picked Up") {
+			// Create row
+			$("#picked-up-table tbody").append("<tr data-key=\"" + key + "\"><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>");
+
+			// Add values to corresponding column
+			var rowTds = $("#picked-up-table")
+				.children()
+				.eq(1)
+				.children("tr")
+				.eq(m)
+				.children("td");
+			var headings = [child.orderNumber, child.created, child.name, child.amount, child.description, child.card, child.last4];
+			for (var i = 0; i < headings.length; i++) {
+				rowTds.eq(i).text(headings[i]);
+			}
+
+			// Append Remove Button
+			var removeButton = $("<button>");
+			removeButton.attr("type", "button");
+			removeButton.attr("data-key", key);
+			removeButton.attr("class", "btn btn-success unpicked-up-button");
+			removeButton.text("Picked Up");
+			$("#picked-up-table tbody tr:last td:last").append(removeButton);
+
+			m++;
+		}
+	}, function(errorObject) {
+		console.log("Errors handled: " + errorObject.code);
+	});
+
+	// Mark order as picked up
+	$(document).on("click", ".picked-up-button", function() {
+		// Update object in database
+		database.ref("/" + $(this).attr("data-key")).update({
+			status: "Picked Up",
+		});
+	});
+
+	// Unmark order as picked up
+	$(document).on("click", ".unpicked-up-button", function() {
+		// Update object in database
+		database.ref("/" + $(this).attr("data-key")).update({
+			status: "Open",
+		});
+	});
+
+	// Update item in table
+	database.ref().on("child_changed", function(snapshot) {
+		var child = snapshot.val();
+		var key = snapshot.key;
+
+		if (child.status === "Picked Up") {
+			// Remove order from open orders table
+			$("#open-table tr[data-key=" + key + "]").remove();
+			n--;
+
+			// Create row
+			$("#picked-up-table tbody").append("<tr data-key=\"" + key + "\"><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>");
+
+			// Add values to corresponding column
+			var rowTds = $("#picked-up-table")
+				.children()
+				.eq(1)
+				.children("tr")
+				.eq(m)
+				.children("td");
+			var headings = [child.orderNumber, child.created, child.name, child.amount, child.description, child.card, child.last4];
+			for (var i = 0; i < headings.length; i++) {
+				rowTds.eq(i).text(headings[i]);
+			}
+
+			// Append Remove Button
+			var removeButton = $("<button>");
+			removeButton.attr("type", "button");
+			removeButton.attr("data-key", key);
+			removeButton.attr("class", "btn btn-success unpicked-up-button");
+			removeButton.text("Picked Up");
+			$("#picked-up-table tbody tr:last td:last").append(removeButton);
+
+			m++;
+		} else if (child.status === "Open") {
+			// Remove order from picked up orders table
+			$("#picked-up-table tr[data-key=" + key + "]").remove();
+			m--;
+
+			// Create row
+			$("#open-table tbody").append("<tr data-key=\"" + key + "\"><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>");
+
+			// Add values to corresponding column
+			var rowTds = $("#open-table")
+				.children()
+				.eq(1)
+				.children("tr")
+				.eq(n)
+				.children("td");
+			var headings = [child.orderNumber, child.created, child.name, child.amount, child.description, child.card, child.last4];
+			for (var i = 0; i < headings.length; i++) {
+				rowTds.eq(i).text(headings[i]);
+			}
+
+			// Append Remove Button
+			var removeButton = $("<button>");
+			removeButton.attr("type", "button");
+			removeButton.attr("data-key", key);
+			removeButton.attr("class", "btn btn-warning picked-up-button");
+			removeButton.text("Picked Up");
+			$("#open-table tbody tr:last td:last").append(removeButton);
+
+			n++;
+		}
+	});
+
+	// Sort Table Ascending
+	$(".fa-caret-down").click(function() {
+		var table, rows, switching, x, y, shouldSwitch;
+		table = document.getElementById($(this).attr("data-table"));
+		switching = true;
+		while (switching) {
+			switching = false;
+			rows = table.getElementsByTagName("TR");
+			for (var i = 1; i < (rows.length - 1); i++) {
+				shouldSwitch = false;
+				x = rows[i].getElementsByTagName("TD")[$(this).attr("data-index")];
+				y = rows[i+1].getElementsByTagName("TD")[$(this).attr("data-index")];
+				if ($(this).attr("data-index") === "1" || $(this).attr("data-index") === "4") {
+					if (parseInt(x.innerHTML) > parseInt(y.innerHTML)) {
+						shouldSwitch= true;
+						break;
+					}
+				}
+				else {
+					if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+						shouldSwitch= true;
+						break;
+					}
+				}
+			}
+			if (shouldSwitch) {
+				rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+				switching = true;
+			}
+		}
+		sortedAsc = true;
+		sortedDesc = false;
+		sortedByAsc = $(this).attr("data-index");
+	});
+
+	// Sort Table Descending
+	$(".fa-caret-up").click(function() {
+		var table, rows, switching, x, y, shouldSwitch;
+		table = document.getElementById($(this).attr("data-table"));
+		switching = true;
+		while (switching) {
+			switching = false;
+			rows = table.getElementsByTagName("TR");
+			for (var i = 1; i < (rows.length - 1); i++) {
+				shouldSwitch = false;
+				x = rows[i].getElementsByTagName("TD")[$(this).attr("data-index")];
+				y = rows[i+1].getElementsByTagName("TD")[$(this).attr("data-index")];
+				if ($(this).attr("data-index") === "1" || $(this).attr("data-index") === "4") {
+					if (parseInt(x.innerHTML) < parseInt(y.innerHTML)) {
+						shouldSwitch= true;
+						break;
+					}
+				}
+				else {
+					if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+						shouldSwitch= true;
+						break;
+					}
+				}
+			}
+			if (shouldSwitch) {
+				rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+				switching = true;
+			}
+		}
+		sortedAsc = false;
+		sortedDesc = true;
+		sortedByAsc = $(this).attr("data-index");
+	});
+});
