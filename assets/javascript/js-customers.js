6eb0ce1f9307b12161b2ca900a67b76d35288304
luis paredes <luis paredes>
2017-04-09 22:31:32 -0400

    updated js file to make customers page only visible if user is logged in

--- assets/javascript/js-customers.js
+++ assets/javascript/js-customers.js
@@ -1,126 +1,144 @@
 $(document).ready(function($) {
+	// Initialize inventory database
 	var config = {
-		apiKey: "AIzaSyBGvojl_lmUCBdcOBC2UlqUZo8KXKzlpa8",
-    authDomain: "project-1-customers.firebaseapp.com",
-    databaseURL: "https://project-1-customers.firebaseio.com",
-    projectId: "project-1-customers",
-    storageBucket: "project-1-customers.appspot.com",
-    messagingSenderId: "686478727174"
+		apiKey: "AIzaSyAD3GCPbQlu4xeI5snnMEU5TMcA_22Wsos",
+		authDomain: "project-1-2329f.firebaseapp.com",
+		databaseURL: "https://project-1-2329f.firebaseio.com",
+		projectId: "project-1-2329f",
+		storageBucket: "project-1-2329f.appspot.com",
+		messagingSenderId: "554989098289"
 	};
-
 	firebase.initializeApp(config);
+	var inventoryDB = firebase.database();
 
-	var database = firebase.database();
-	var imgName = "";
-	var updateImgName = "";
-	var n = 0;
-	var sortedAsc = false;
-	var sortedDesc = false;
+	firebase.auth().onAuthStateChanged(function(firebaseUser) {
+		if (firebaseUser) {
+			// Initialize customers database
+			var configCustomers = {
+				apiKey: "AIzaSyBGvojl_lmUCBdcOBC2UlqUZo8KXKzlpa8",
+		    authDomain: "project-1-customers.firebaseapp.com",
+		    databaseURL: "https://project-1-customers.firebaseio.com",
+		    projectId: "project-1-customers",
+		    storageBucket: "project-1-customers.appspot.com",
+		    messagingSenderId: "686478727174"
+			};
+			var customersApp = firebase.initializeApp(configCustomers, "Customers");
+			var customersDB = customersApp.database();
 
-	// Add item to table
-	database.ref().on("child_added", function(snapshot) {
-		var child = snapshot.val();
-		var key = snapshot.key;
+			var imgName = "";
+			var updateImgName = "";
+			var n = 0;
+			var sortedAsc = false;
+			var sortedDesc = false;
 
-		// Create row
-		$("#customers-table tbody").append("<tr data-key=\"" + key + "\"><td></td><td></td><td></td><td></td><td></td></tr>");
+			// Add item to table
+			customersDB.ref().on("child_added", function(snapshot) {
+				var child = snapshot.val();
+				var key = snapshot.key;
 
-		// Add values to corresponding column
-		var rowTds = $("#customers-table")
-			.children()
-			.eq(1)
-			.children("tr")
-			.eq(n)
-			.children("td");
-		var headings = [key, child.created, child.name, child.email, child.phone];
-		for (var i = 0; i < headings.length; i++) {
-			rowTds.eq(i).text(headings[i]);
-		}
+				// Create row
+				$("#customers-table tbody").append("<tr data-key=\"" + key + "\"><td></td><td></td><td></td><td></td><td></td></tr>");
 
-		n++;
-	}, function(errorObject) {
-		console.log("Errors handled: " + errorObject.code);
-	});
-
-	// Sort Table Ascending
-	$(".fa-caret-down").click(function() {
-		var table, rows, switching, x, y, shouldSwitch;
-		table = document.getElementById("customers-table");
-		switching = true;
-		while (switching) {
-			switching = false;
-			rows = table.getElementsByTagName("TR");
-			for (var i = 1; i < (rows.length - 1); i++) {
-				shouldSwitch = false;
-				x = rows[i].getElementsByTagName("TD")[$(this).attr("data-index")];
-				y = rows[i+1].getElementsByTagName("TD")[$(this).attr("data-index")];
-				if ($(this).attr("data-index") === "4") {
-					if (parseInt(x.innerHTML) > parseInt(y.innerHTML)) {
-						shouldSwitch= true;
-						break;
-					}
+				// Add values to corresponding column
+				var rowTds = $("#customers-table")
+					.children()
+					.eq(1)
+					.children("tr")
+					.eq(n)
+					.children("td");
+				var headings = [key, child.created, child.name, child.email, child.phone];
+				for (var i = 0; i < headings.length; i++) {
+					rowTds.eq(i).text(headings[i]);
 				}
-				else if ($(this).attr("data-index") === "1") {
-					if (moment(x.innerHTML, "MMM DD, YYYY hh:mm:ss").format("X") > moment(y.innerHTML, "MMM DD, YYYY hh:mm:ss").format("X")) {
-						shouldSwitch= true;
-						break;
-					}
-				}
-				else {
-					if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-						shouldSwitch= true;
-						break;
-					}
-				}
-			}
-			if (shouldSwitch) {
-				rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+
+				n++;
+			}, function(errorObject) {
+				console.log("Errors handled: " + errorObject.code);
+			});
+
+			// Sort Table Ascending
+			$(".fa-caret-down").click(function() {
+				var table, rows, switching, x, y, shouldSwitch;
+				table = document.getElementById("customers-table");
 				switching = true;
-			}
-		}
-		sortedAsc = true;
-		sortedDesc = false;
-		sortedByAsc = $(this).attr("data-index");
-	});
+				while (switching) {
+					switching = false;
+					rows = table.getElementsByTagName("TR");
+					for (var i = 1; i < (rows.length - 1); i++) {
+						shouldSwitch = false;
+						x = rows[i].getElementsByTagName("TD")[$(this).attr("data-index")];
+						y = rows[i+1].getElementsByTagName("TD")[$(this).attr("data-index")];
+						if ($(this).attr("data-index") === "4") {
+							if (parseInt(x.innerHTML) > parseInt(y.innerHTML)) {
+								shouldSwitch= true;
+								break;
+							}
+						}
+						else if ($(this).attr("data-index") === "1") {
+							if (moment(x.innerHTML, "MMM DD, YYYY hh:mm:ss").format("X") > moment(y.innerHTML, "MMM DD, YYYY hh:mm:ss").format("X")) {
+								shouldSwitch= true;
+								break;
+							}
+						}
+						else {
+							if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+								shouldSwitch= true;
+								break;
+							}
+						}
+					}
+					if (shouldSwitch) {
+						rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+						switching = true;
+					}
+				}
+				sortedAsc = true;
+				sortedDesc = false;
+				sortedByAsc = $(this).attr("data-index");
+			});
 
-	// Sort Table Descending
-	$(".fa-caret-up").click(function() {
-		var table, rows, switching, x, y, shouldSwitch;
-		table = document.getElementById("customers-table");
-		switching = true;
-		while (switching) {
-			switching = false;
-			rows = table.getElementsByTagName("TR");
-			for (var i = 1; i < (rows.length - 1); i++) {
-				shouldSwitch = false;
-				x = rows[i].getElementsByTagName("TD")[$(this).attr("data-index")];
-				y = rows[i+1].getElementsByTagName("TD")[$(this).attr("data-index")];
-				if ($(this).attr("data-index") === "4") {
-					if (parseInt(x.innerHTML) < parseInt(y.innerHTML)) {
-						shouldSwitch= true;
-						break;
-					}
-				}
-				else if ($(this).attr("data-index") === "1") {
-					if (moment(x.innerHTML, "MMM DD, YYYY hh:mm:ss").format("X") < moment(y.innerHTML, "MMM DD, YYYY hh:mm:ss").format("X")) {
-						shouldSwitch= true;
-						break;
-					}
-				}
-				else {
-					if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-						shouldSwitch= true;
-						break;
-					}
-				}
-			}
-			if (shouldSwitch) {
-				rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+			// Sort Table Descending
+			$(".fa-caret-up").click(function() {
+				var table, rows, switching, x, y, shouldSwitch;
+				table = document.getElementById("customers-table");
 				switching = true;
-			}
+				while (switching) {
+					switching = false;
+					rows = table.getElementsByTagName("TR");
+					for (var i = 1; i < (rows.length - 1); i++) {
+						shouldSwitch = false;
+						x = rows[i].getElementsByTagName("TD")[$(this).attr("data-index")];
+						y = rows[i+1].getElementsByTagName("TD")[$(this).attr("data-index")];
+						if ($(this).attr("data-index") === "4") {
+							if (parseInt(x.innerHTML) < parseInt(y.innerHTML)) {
+								shouldSwitch= true;
+								break;
+							}
+						}
+						else if ($(this).attr("data-index") === "1") {
+							if (moment(x.innerHTML, "MMM DD, YYYY hh:mm:ss").format("X") < moment(y.innerHTML, "MMM DD, YYYY hh:mm:ss").format("X")) {
+								shouldSwitch= true;
+								break;
+							}
+						}
+						else {
+							if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+								shouldSwitch= true;
+								break;
+							}
+						}
+					}
+					if (shouldSwitch) {
+						rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+						switching = true;
+					}
+				}
+				sortedAsc = false;
+				sortedDesc = true;
+				sortedByAsc = $(this).attr("data-index");
+			});
+		} else {
+			window.location = "products.html";
 		}
-		sortedAsc = false;
-		sortedDesc = true;
-		sortedByAsc = $(this).attr("data-index");
 	});
 });
