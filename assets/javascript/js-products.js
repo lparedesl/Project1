276f2e6200a59f25126b00e602d53e8c337a0ddd
luis paredes <luis paredes>
2017-04-10 17:26:10 -0400

    updated js to show if item is out of stock, set cart quantity limit based on inventory. Added test sign in/out button

--- assets/javascript/js-products.js
+++ assets/javascript/js-products.js
@@ -19,6 +19,143 @@ $(document).ready(function($) {
 	    return this.charAt(0).toUpperCase() + this.slice(1);
 	};
 
+	$("#btn-login").click(function(event) {
+		$("#login-window").removeClass("hidden");
+		$(".update-bg").removeClass("hidden");
+	});
+
+	$("#login").click(function(event) {
+		event.preventDefault();
+		var email = $("#input-username").val().trim();
+		var pass = $("#input-password").val().trim();
+		var auth = firebase.auth();
+		auth.signInWithEmailAndPassword(email, pass)
+		.catch(function(error) {
+			console.log(error.message);
+			var wrongPass = "The password is invalid or the user does not have a password.";
+			var noUser = "There is no user record corresponding to this identifier. The user may have been deleted.";
+			var badEmail = "The email address is badly formatted.";
+
+			$("#login-window").css({
+				height: "52vh",
+			});
+			if (error.message === wrongPass) {
+				$("#user-nonexistent").addClass("hidden");
+				$("#bad-email").addClass("hidden");
+				$("#password-incorrect").removeClass("hidden");
+			} else if (error.message === noUser) {
+				$("#password-incorrect").addClass("hidden");
+				$("#bad-email").addClass("hidden");
+				$("#user-nonexistent").removeClass("hidden");
+			} else if (error.message === badEmail) {
+				$("#password-incorrect").addClass("hidden");
+				$("#user-nonexistent").addClass("hidden");
+				$("#bad-email").removeClass("hidden");
+			}
+		});
+		$("#input-username").val("");
+		$("#input-password").val("");
+		$(".update-bg").addClass("hidden");
+	});
+
+	$("#signup").click(function(event) {
+		event.preventDefault();
+		$("#login-window").css({
+			height: "46vh",
+		});
+		$("#signup-window").removeClass("hidden");
+		$(".update-bg").removeClass("hidden");
+		$("#user-nonexistent").addClass("hidden");
+		$("#password-incorrect").addClass("hidden");
+	});
+
+	$("#register-user").click(function(event) {
+		event.preventDefault();
+		if ($("#password-new").val().trim() !== $("#confirm-password-new").val().trim() || $("#password-new").val().trim() === "" || $("#confirm-password-new").val().trim() === "") {
+			$("#signup-window").css({
+				height: "55vh",
+			});
+			$("#pass-min-length").addClass("hidden");
+			$("#email-exists").addClass("hidden");
+			$("#invalid-email").addClass("hidden");
+			$("#password-mismatch").removeClass("hidden");
+		} else {
+			var email = $("#email-new").val().trim();
+			var pass = $("#password-new").val().trim();
+			var auth = firebase.auth();
+			auth.createUserWithEmailAndPassword(email, pass)
+			.catch(function(error) {
+				console.log(error.message);
+				var passLength = "Password should be at least 6 characters";
+				var userExists = "The email address is already in use by another account.";
+				var badEmail = "The email address is badly formatted.";
+
+				$("#signup-window").css({
+					height: "55vh",
+				});
+				$("#password-mismatch").addClass("hidden");
+				if (error.message === badEmail) {
+					$("#pass-min-length").addClass("hidden");
+					$("#email-exists").addClass("hidden");
+					$("#invalid-email").removeClass("hidden");
+				} else if (error.message === userExists) {
+					$("#pass-min-length").addClass("hidden");
+					$("#invalid-email").addClass("hidden");
+					$("#email-exists").removeClass("hidden");
+				} else if (error.message === passLength) {
+					$("#pass-min-length").removeClass("hidden");
+					$("#email-exists").addClass("hidden");
+					$("#invalid-email").addClass("hidden");
+				}
+			});
+		}
+	});
+
+	$("#close-signup-window").click(function(event) {
+		event.preventDefault();
+		$("#signup-window").css({
+			height: "50vh",
+		});
+		$("#signup-window").addClass("hidden");
+		$(".update-bg").addClass("hidden");
+		$("#password-mismatch").addClass("hidden");
+		$("#email-exists").addClass("hidden");
+		$("#pass-min-length").addClass("hidden");
+		$("#invalid-email").addClass("hidden");
+		var inputs = [$("#first-name-new"), $("#last-name-new"), $("#email-new"), $("#username-new"), $("#password-new"), $("#confirm-password-new")];
+		for (var j = 0; j < inputs.length; j++) {
+			inputs[j].val("");
+		}
+	});
+
+	$("#btn-logout").click(function(event) {
+		firebase.auth().signOut();
+	});
+
+	firebase.auth().onAuthStateChanged(function(firebaseUser) {
+		if (firebaseUser) {
+			$(".update-bg").addClass("hidden");
+			$("#login-window").addClass("hidden");
+			$("#btn-login").addClass("hidden");
+			$("#password-incorrect").addClass("hidden");
+			$("#user-nonexistent").addClass("hidden");
+			$("#bad-email").addClass("hidden");
+			$("#login-window").css({
+				height: "46vh",
+			});
+			$("#signup-window").addClass("hidden");
+			$("#password-mismatch").addClass("hidden");
+			$("#email-exists").addClass("hidden");
+			$("#pass-min-length").addClass("hidden");
+			$("#invalid-email").addClass("hidden");
+			$("#user").text(firebaseUser.email + " ");
+			$("#userMenu").removeClass("hidden");
+		} else {
+			$("#btn-login").removeClass("hidden");
+			$("#userMenu").addClass("hidden");
+		}
+	});
+
 	// Add item to page
 	database.ref().on("child_added", function(snapshot) {
 		var child = snapshot.val();
@@ -41,11 +178,16 @@ $(document).ready(function($) {
 	  	itemPrice.text("$" + price);
 
 	  	var addToCart = $("<button>");
-	  	addToCart.addClass("btn btn-primary pull-right");
 	  	addToCart.attr("type", "button");
 	  	addToCart.attr("id", "add-to-cart");
 	  	addToCart.attr("data-key", key);
-	  	addToCart.text("Add to cart");
+	  	if (child.quantity > 0) {
+	  		addToCart.addClass("btn btn-primary pull-right");
+	  		addToCart.text("Add to cart");
+	  	} else {
+	  		addToCart.addClass("btn btn-danger disabled pull-right");
+	  		addToCart.text("Out of stock");
+	  	}
 
 	  	var buyContent = $("<div>");
 	  	buyContent.addClass("clearfix");
@@ -107,6 +249,7 @@ $(document).ready(function($) {
 			for (var i = 0; i < itemsList.length; i++) {
 				var name = $("<p>").text(sv[itemsList[i]].name);
 				var price = $("<p>").text("$" + (parseFloat(sv[itemsList[i]].price) * parseInt(quantitiesList[i])));
+
 				var quantity = $("<select>");
 				quantity.addClass("form-control");
 				quantity.attr("data-index", i);
@@ -119,11 +262,13 @@ $(document).ready(function($) {
 					option.text(j+1);
 					quantity.append(option);
 				}
-				var b = $("<button class='btn btn-default delete'>").text("Remove").attr("data-index", i);
+
+				var remove = $("<button class='btn btn-default delete'>").text("Remove").attr("data-index", i);
 				totalPrice += (parseFloat(sv[itemsList[i]].price) * parseInt(quantitiesList[i]));
+
 	        	name.append(price);
 	        	name.append(quantity);
-	        	name.append(b);
+	        	name.append(remove);
 				$("#cart-items").append(name);
 				$("#total-price").text("Total: $" + totalPrice);
 			}
@@ -135,43 +280,69 @@ $(document).ready(function($) {
 	// Add item to cart
 	$(document).on("click", "#add-to-cart", function() {
 		event.preventDefault();
-		var items = JSON.parse(localStorage.getItem("items"));
-		if (!Array.isArray(items)) {
-			items = [];
-		}
-		totalPrice = 0;
-		var quantities = JSON.parse(localStorage.getItem("quantities"));
+		if ($(this).text() === "Add to cart") {
+			var items = JSON.parse(localStorage.getItem("items"));
+			if (!Array.isArray(items)) {
+				items = [];
+			}
+			totalPrice = 0;
+			var quantities = JSON.parse(localStorage.getItem("quantities"));
 
-		// If item is not in cart
-		if (items.indexOf($(this).attr("data-key")) === -1) {
-			var itemKey = $(this).attr("data-key");
-			var quantity = 1;
-			itemsList.push(itemKey);
-			quantitiesList.push(quantity);
-			localStorage.setItem("items", JSON.stringify(itemsList));
-			localStorage.setItem("quantities", JSON.stringify(quantitiesList));
-			showCart();
+			// If item is not in cart
+			if (items.indexOf($(this).attr("data-key")) === -1) {
+				var itemKey = $(this).attr("data-key");
+				var quantity = 1;
+				itemsList.push(itemKey);
+				quantitiesList.push(quantity);
+				localStorage.setItem("items", JSON.stringify(itemsList));
+				localStorage.setItem("quantities", JSON.stringify(quantitiesList));
+				showCart();
 
-		// If item is in the cart
-		} else {
-			var itemIndex = items.indexOf($(this).attr("data-key"));
-			var newQuantity = parseInt(quantities[itemIndex]);
-			newQuantity++;
-			quantities[itemIndex] = newQuantity;
-			localStorage.setItem("quantities", JSON.stringify(quantities));
-			showCart();
+			// If item is in the cart
+			} else {
+				var itemIndex = items.indexOf($(this).attr("data-key"));
+				var newQuantity = parseInt(quantities[itemIndex]);
+				newQuantity++;
+				quantities[itemIndex] = newQuantity;
+				localStorage.setItem("quantities", JSON.stringify(quantities));
+				showCart();
+			}
 		}
 	});
 
 	// Update item quantity
 	$(document).on("change", "select", function() {
-		totalPrice = 0;
-		var itemIndex = $(this).attr("data-index");
-		var quantities = JSON.parse(localStorage.getItem("quantities"));
-		var newQuantity = parseInt($(this).val());
-		quantities[itemIndex] = newQuantity;
-		localStorage.setItem("quantities", JSON.stringify(quantities));
-		showCart();
+		var self = $(this);
+		database.ref().on("value", function(snapshot) {
+			var sv = snapshot.val();
+			var keys = Object.keys(sv);
+			var itemIndex = self.attr("data-index");
+			var items = JSON.parse(localStorage.getItem("items"));
+			var thisKey = items[itemIndex];
+			var quantities = JSON.parse(localStorage.getItem("quantities"));
+
+			// If inventory is less than quantity selected, set value to max and show alert
+			if (sv[thisKey].quantity < parseInt(self.val())) {
+				self.val(sv[thisKey].quantity);
+				var txt = $("<p>");
+				txt.attr("data-index", itemIndex);
+				txt.text(sv[thisKey].name + "s " + "currently in stock: " + sv[thisKey].quantity);
+				$("#quantity-alert").append(txt);
+				$("#quantity-alert").removeClass("hidden");
+			} else {
+				var numOfAlerts = $("#quantity-alert").children().length;
+				if (numOfAlerts === 1) {
+					$("#quantity-alert").addClass("hidden");
+				}
+				$("#quantity-alert").children("p[data-index=\"" + itemIndex + "\"]").remove();
+			}
+
+			var newQuantity = parseInt(self.val());
+			totalPrice = 0;
+			quantities[itemIndex] = newQuantity;
+			localStorage.setItem("quantities", JSON.stringify(quantities));
+			showCart();
+		});
 	});
 
 	// Remove item from cart
